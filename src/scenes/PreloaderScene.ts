eca2fb5a1d812a1668f24a0c19a5935aee5e75cc
paulasantamaria <m_paula_santamaria@live.com>
2022-04-02 13:33:51 -0300

    Add upgrade assets

--- src/scenes/PreloaderScene.ts
+++ src/scenes/PreloaderScene.ts
@@ -8,9 +8,15 @@ export default class Preloader extends Phaser.Scene {
 
     preload(): void {
         this.load.image('spaceship', 'assets/spaceship.png');
+        
         this.load.image('bullet-01', 'assets/bullet-01.png');
         this.load.image('bullet-02', 'assets/bullet-02.png');
+        
         this.load.image('asteroid', 'assets/asteroid.png');
+        
+        this.load.image('simple-upgrade', 'assets/simple-upgrade.png');
+        this.load.image('double-upgrade', 'assets/double-upgrade.png');
+        this.load.image('bubble-upgrade', 'assets/bubble-upgrade.png');
     }
 
     create(): void {
